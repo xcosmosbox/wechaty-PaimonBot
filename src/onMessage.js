1bcf9496e32a2ce50ede747a7359a20f4df9637d
SanCheck <2162381070@qq.com>
2021-08-05 23:07:26 +0800

    对onMessage模块进行更新

---
+++ src/onMessage.js
@@ -0,0 +1,189 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
+exports.onMessage = void 0;
+var wechaty_1 = require("wechaty");
+var fs = require("fs");
+function onMessage(message) {
+    return __awaiter(this, void 0, void 0, function () {
+        var contact, text, room, infoFile, friendAndRoomJson, myName, start_length, strStart, strEnd, start_length_1, strEnd_1, room_name, city_name, urlfile, userJson, data, dataStr, image_path_in_dir, fileBox, error_1, QAFile, QAJson, contact_name, city_name, urlfile, userJson, data, dataStr, image_path_in_dir, fileBox, error_2, QAFile, QAJson;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    contact = message.talker();
+                    text = message.text();
+                    room = message.room();
+                    infoFile = "../json/friendAndRoomInfo.json";
+                    friendAndRoomJson = JSON.parse(fs.readFileSync(infoFile, 'utf-8'));
+                    myName = friendAndRoomJson["myName"];
+                    start_length = 2 + myName.length;
+                    strStart = text.charAt(0) + text.charAt(1) + text.charAt(2) + text.charAt(3) + text.charAt(4);
+                    strEnd = text.substring(start_length, text.length);
+                    //消息如果是自己发的，直接return，别浪费内存了
+                    if (message.self()) {
+                        console.log("消息是我自己发的不用管");
+                        return [2 /*return*/, 0];
+                    }
+                    // print message
+                    console.log('on message: ' + message.toString()); //打印信息内容
+                    console.log("这是 strEnd 的内容：" + strEnd);
+                    console.log("这是 strEnd 的长度：" + strEnd.length);
+                    if (!room) return [3 /*break*/, 6];
+                    start_length_1 = 2 + myName.length;
+                    strEnd_1 = text.substring(start_length_1, text.length);
+                    return [4 /*yield*/, room.topic()];
+                case 1:
+                    room_name = _a.sent();
+                    console.log(room_name);
+                    console.log("群聊时是谁在说话：" + contact.name());
+                    if ((friendAndRoomJson["room_Keys"])[room_name] == undefined) {
+                        console.log("这不是我关注的群聊");
+                        return [2 /*return*/, 0]; //找不到说明不是关注的群聊，直接返回
+                    }
+                    //判断是否为at我，不是也不需要管这个信息了，直接return
+                    console.log(strStart == ("@" + myName));
+                    if (strStart != ("@" + myName)) {
+                        console.log("没有at我");
+                        return [2 /*return*/, 0];
+                    }
+                    if (!isWeatherMsg(strEnd_1)) return [3 /*break*/, 5];
+                    city_name = strEnd_1.charAt(0) + strEnd_1.charAt(1);
+                    urlfile = "../PyMode/WeatherCrawlMode/out/city_json/" + city_name + ".json";
+                    if (!fs.existsSync(urlfile)) return [3 /*break*/, 5];
+                    _a.label = 2;
+                case 2:
+                    _a.trys.push([2, 3, , 5]);
+                    userJson = JSON.parse(fs.readFileSync(urlfile, 'utf-8'));
+                    data = userJson[city_name];
+                    dataStr = JSON.stringify(data);
+                    room.say("@" + (contact === null || contact === void 0 ? void 0 : contact.name()) + " " + city_name + "今日天气： \n" + dataStr); // 输出数据
+                    image_path_in_dir = '../PyMode/WeatherCrawlMode/out/city_pic/' + city_name + '.png' //获取天气图片的地址
+                    ;
+                    fileBox = wechaty_1.FileBox.fromFile(image_path_in_dir);
+                    room.say(fileBox); //发送图片
+                    return [2 /*return*/, 0];
+                case 3:
+                    error_1 = _a.sent();
+                    room.say("@" + (contact === null || contact === void 0 ? void 0 : contact.name()) + " " + "你在赣神谟？");
+                    return [4 /*yield*/, Sleep(1000)];
+                case 4:
+                    _a.sent();
+                    room.say("这个城市压根不存在！");
+                    return [2 /*return*/, 0];
+                case 5:
+                    QAFile = "../json/QA.json" //json文件的存储地址
+                    ;
+                    QAJson = JSON.parse(fs.readFileSync(QAFile, 'utf-8'));
+                    if (QAJson[strEnd_1] != undefined) //不是undefined说明有对应的问答
+                     {
+                        room.say("@" + (contact === null || contact === void 0 ? void 0 : contact.name()) + " " + QAJson[strEnd_1]); //将对应的问答回复他人
+                    }
+                    else {
+                        room.say("@" + (contact === null || contact === void 0 ? void 0 : contact.name()) + " " + "我不是很懂你在说什么。。。");
+                    }
+                    return [3 /*break*/, 11];
+                case 6:
+                    contact_name = contact === null || contact === void 0 ? void 0 : contact.name();
+                    if ((friendAndRoomJson["friend_Keys"])[contact_name] == undefined)
+                        return [2 /*return*/, 0]; //找不到说明不是关注的私聊，直接返回
+                    console.log(contact_name);
+                    if (!isWeatherMsg(text)) return [3 /*break*/, 10];
+                    city_name = text.charAt(0) + text.charAt(1);
+                    urlfile = "../PyMode/WeatherCrawlMode/out/city_json/" + city_name + ".json";
+                    if (!(fs.existsSync(urlfile) && contact != null)) return [3 /*break*/, 10];
+                    _a.label = 7;
+                case 7:
+                    _a.trys.push([7, 8, , 10]);
+                    userJson = JSON.parse(fs.readFileSync(urlfile, 'utf-8'));
+                    data = userJson[city_name];
+                    dataStr = JSON.stringify(data);
+                    contact.say(city_name + "今日天气： \n" + dataStr); // 输出数据
+                    image_path_in_dir = '../PyMode/WeatherCrawlMode/out/city_pic/' + city_name + '.png' //获取天气图片的地址
+                    ;
+                    fileBox = wechaty_1.FileBox.fromFile(image_path_in_dir);
+                    contact.say(fileBox); //发送图片
+                    return [2 /*return*/, 0];
+                case 8:
+                    error_2 = _a.sent();
+                    contact.say("你在赣神谟？");
+                    return [4 /*yield*/, Sleep(1000)];
+                case 9:
+                    _a.sent();
+                    contact.say("这个城市压根不存在！");
+                    return [2 /*return*/, 0];
+                case 10:
+                    QAFile = "../json/QA.json" //json文件的存储地址
+                    ;
+                    QAJson = JSON.parse(fs.readFileSync(QAFile, 'utf-8'));
+                    if (QAJson[text] != undefined && contact != null) //不是undefined说明有对应的问答
+                     {
+                        contact.say(QAJson[text]); //将对应的问答回复他人
+                    }
+                    else {
+                        contact === null || contact === void 0 ? void 0 : contact.say("我不是很懂你在说什么。。。");
+                    }
+                    _a.label = 11;
+                case 11: return [2 /*return*/];
+            }
+        });
+    });
+}
+exports.onMessage = onMessage;
+//使用这个函数判断用户是否想要得到天气数据
+function isWeatherMsg(strEnd) {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            //如果长度为4
+            if (strEnd.length == 4) { //如果第三个和第四个字符是'天气'
+                if (strEnd.charAt(2) == '天' && strEnd.charAt(3) == '气') {
+                    return [2 /*return*/, true];
+                }
+                else
+                    return [2 /*return*/, false];
+            }
+            else
+                return [2 /*return*/, false];
+            return [2 /*return*/];
+        });
+    });
+}
+//休眠程序
+var Sleep = function (ms) {
+    return new Promise(function (resolve) { return setTimeout(resolve, ms); });
+};
+// module.exports = (bot: Wechaty) => {}
--- src/onMessage.ts
+++ src/onMessage.ts
@@ -5,17 +5,21 @@ import * as fs from 'fs';
 
 export async function onMessage(message:Message) {
     //初始化数据，获取信息的聊天人、文本信息、房间信息等信息
-    const contact = message.from() //非名字，而是一个Contact对象
-    const text = message.text()  
-    const room = message.room() //一个Room对象
-    const infoFile = "../json/friendAndRoomInfo.json" //json文件的存储地址
+    const contact = message.talker(); //非名字，而是一个Contact对象
+    const text = message.text();  
+    const room = message.room(); //一个Room对象
+    const infoFile = "../json/friendAndRoomInfo.json"; //json文件的存储地址
     const friendAndRoomJson = JSON.parse(fs.readFileSync(infoFile,'utf-8')); //json文件的读取
-    const myName = friendAndRoomJson["myName"] //获得使用者登录的微信号的用户名
-    const start_length = 2 + myName.length //在信息中，”@“+”myName“+”【一个空格】“，因此长度是 （2+myName.length）
-    const strEnd = text.substring(start_length,text.length) //截取剩余的信息
+    const myName = friendAndRoomJson["myName"]; //获得使用者登录的微信号的用户名
+    const strStart = text.charAt(0)+text.charAt(1)+text.charAt(2)+text.charAt(3)+text.charAt(4)
+    
     
     //消息如果是自己发的，直接return，别浪费内存了
-    if(message.self()) return 0
+    if(message.self())
+    {
+        console.log("消息是我自己发的不用管");
+        return 0;
+    }
     
     // print message
     console.log('on message: ' + message.toString()); //打印信息内容
@@ -25,11 +29,25 @@ export async function onMessage(message:Message) {
     {
         //说明是群聊
         //返回undefined说明不在关注列表，直接返回return
-        const room_name = room.topic()+""  //之所以要加双引号双因为，topic返回的是一个非引用类型，需要加双引号变为string，string是一个引用类型
-        if(friendAndRoomJson.room_Keys[room_name] == undefined) return 0//找不到说明不是关注的群聊，直接返回
+        const start_length = 2 + myName.length; //在信息中，”@“+”myName“+”【一个空格】“，因此长度是 （2+myName.length）
+        const strEnd = text.substring(start_length,text.length); //截取剩余的信息
+        const room_name = await room.topic();  //之所以要加双引号双因为，topic返回的是一个非引用类型，需要加双引号变为string，string是一个引用类型
+        // console.log(room_name)
+        // console.log("群聊时是谁在说话："+contact.name())
+        
+        if((friendAndRoomJson["room_Keys"])[room_name] == undefined)
+        {
+            console.log("这不是我关注的群聊");
+            return 0;//找不到说明不是关注的群聊，直接返回
+        } 
 
         //判断是否为at我，不是也不需要管这个信息了，直接return
-        if(!message.mentionSelf()) return 0
+        // console.log(strStart == ("@"+myName))
+        if(strStart != ("@"+myName))
+        {
+            // console.log("没有at我");
+            return 0
+        } 
 
         //对于天气爬虫的使用
         if(isWeatherMsg(strEnd))
@@ -46,13 +64,13 @@ export async function onMessage(message:Message) {
                     let userJson = JSON.parse(fs.readFileSync(urlfile,'utf-8'));
                     const data = userJson[city_name];
                     const dataStr = JSON.stringify(data)
-                    room.say(`@${contact?.name} `+city_name+"今日天气： \n"+dataStr) // 输出数据
+                    room.say(`@${contact?.name()} `+city_name+"今日天气： \n"+dataStr) // 输出数据
                     const image_path_in_dir = '../PyMode/WeatherCrawlMode/out/city_pic/' + city_name + '.png' //获取天气图片的地址
                     const fileBox = FileBox.fromFile(image_path_in_dir)
                     room.say(fileBox) //发送图片
                     return 0
                 } catch (error) {
-                    room.say(`@${contact?.name} `+"你在赣神谟？")
+                    room.say(`@${contact?.name()} `+"你在赣神魔？")
                     await Sleep(1000)
                     room.say("这个城市压根不存在！")
                     return 0
@@ -60,33 +78,34 @@ export async function onMessage(message:Message) {
             }
         }
 
-        //到了此处却没有被return，说明进入了闲聊模块
+        //到了此处却没有被return，说明进入了固定问答模块
         const QAFile = "../json/QA.json" //json文件的存储地址
         const QAJson = JSON.parse(fs.readFileSync(QAFile,'utf-8')); //json文件的读取
         if(QAJson[strEnd] != undefined) //不是undefined说明有对应的问答
         {
-            room.say(`@${contact?.name} `+QAJson[strEnd]) //将对应的问答回复他人
+            room.say(`@${contact?.name()} `+QAJson[strEnd]) //将对应的问答回复他人
         }
         else
         {
-            room.say(`@${contact?.name} `+"我不是很懂你在说什么。。。")
+            room.say(`@${contact?.name()} `+"我不是很懂你在说什么。。。")
         }
         
 
 
-    }
-    else
+    }   ///////////////////////////////////////群聊和私聊分界线///////////////////////////////////////
+    else///////////////////////////////////////群聊和私聊分界线///////////////////////////////////////
     {
         //说明是私聊
         //返回undefined说明不在关注列表，直接返回return
-        const contact_name = contact?.name()+""  //之所以要加双引号双因为，name返回的是一个非引用类型，需要加双引号变为string，string是一个引用类型
-        if(friendAndRoomJson.friend_Keys[contact_name] == undefined) return 0//找不到说明不是关注的私聊，直接返回
+        const contact_name = contact?.name(); //之所以要加双引号双因为，name返回的是一个非引用类型，需要加双引号变为string，string是一个引用类型
+        if((friendAndRoomJson["friend_Keys"])[contact_name] == undefined) return 0//找不到说明不是关注的私聊，直接返回
+        console.log(contact_name)
 
         //对于天气爬虫的使用
-        if(isWeatherMsg(strEnd))
+        if(isWeatherMsg(text))
         {
             //取第一个和第二个字符组成为城市名
-            const city_name = strEnd.charAt(0) + strEnd.charAt(1)
+            const city_name = text.charAt(0) + text.charAt(1)
             //取得json文件
             const urlfile = "../PyMode/WeatherCrawlMode/out/city_json/" + city_name + ".json" 
             //开始对天气信息进行输出
@@ -113,12 +132,12 @@ export async function onMessage(message:Message) {
             }
         }
 
-        //到了此处却没有被return，说明进入了闲聊模块
+        //到了此处却没有被return，说明进入了固定问答模块
         const QAFile = "../json/QA.json" //json文件的存储地址
         const QAJson = JSON.parse(fs.readFileSync(QAFile,'utf-8')); //json文件的读取
-        if(QAJson[strEnd]!=undefined && contact!=null) //不是undefined说明有对应的问答
+        if(QAJson[text]!=undefined && contact!=null) //不是undefined说明有对应的问答
         {
-            contact.say(QAJson[strEnd]) //将对应的问答回复他人
+            contact.say(QAJson[text]) //将对应的问答回复他人
         }
         else
         {
