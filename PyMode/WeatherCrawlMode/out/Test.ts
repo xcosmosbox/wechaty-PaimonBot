1769db8c578fc346f93bd659433fcabb72f73cc2
SanCheck <2162381070@qq.com>
2021-08-22 12:17:48 +0800

    update

--- PyMode/WeatherCrawlMode/out/Test.ts
+++ PyMode/WeatherCrawlMode/out/Test.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 const city_name_list_File = "./cityNumber.json" //json文件的存储地址
 const city_name_list_Json = JSON.parse(fs.readFileSync(city_name_list_File,'utf-8')); //json文件的读取
 const city_name = "成都"
-// const num = 0
+// const num = 0  test
 let i;
 for( i=0;i<462;i++)
 {
